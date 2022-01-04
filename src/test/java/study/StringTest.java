ac2967569b56b693811ba2b1170ec91a3e283be2
DevRunner21 <devrunner21@gmail.com>
2022-01-04 23:26:55 +0900

    refactor: replace() 테스트에 명세를 작성하라
    Signed-off-by: DevRunner21 <devrunner21@gmail.com>

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -8,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StringTest {
 
     @Test
+    @DisplayName("replace()는 지정한 문자열을 다른 문자열로 교체한다.")
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
