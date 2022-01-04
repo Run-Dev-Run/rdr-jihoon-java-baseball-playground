f135acca1dbbfe82f54a255419113ea5736ad7ce
DevRunner21 <devrunner21@gmail.com>
2022-01-04 23:38:47 +0900

    test: substring()의 테스트 케이스를 작성하라
    Signed-off-by: DevRunner21 <devrunner21@gmail.com>

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -9,9 +9,24 @@ public class StringTest {
 
     @Test
     @DisplayName("replace()는 지정한 문자열을 다른 문자열로 교체한다.")
-    void replace() {
+    void test_replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    @DisplayName("split()는 beginIndex 이상 endIndex 미만의 문자열을 잘라내서 반환한다.")
+    void test_split() {
+        // given
+        String data = "(1,2)";
+        int beginIndex = 1;
+        int endIndex = 4;
+
+        // when
+        String actual = data.substring(beginIndex, endIndex);
+
+        // then
+        assertThat(actual).isEqualTo("1,2");
     }
 
 }
