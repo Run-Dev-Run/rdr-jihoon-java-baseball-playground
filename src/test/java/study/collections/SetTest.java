9afd8e567d690c3948c644e8e07703e0360fb05e
DevRunner21 <devrunner21@gmail.com>
2022-01-05 00:12:23 +0900

    test: Collection Set Test를 위한 Class를 세팅하라
    Signed-off-by: DevRunner21 <devrunner21@gmail.com>

---
+++ src/test/java/study/collections/SetTest.java
@@ -0,0 +1,25 @@
+package study.collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SetTest {
+  private Set<Integer> numbers;
+
+  @BeforeEach
+  void setUp() {
+    numbers = new HashSet<>();
+    numbers.add(1);
+    numbers.add(1);
+    numbers.add(2);
+    numbers.add(3);
+  }
+
+  // Test Case 구현
+}
