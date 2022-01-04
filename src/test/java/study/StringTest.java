a1cf4cfe288a8cd45974d5569305fd20d8824569
DevRunner21 <devrunner21@gmail.com>
2022-01-05 00:03:57 +0900

    test: charAt()의 테스트 케이스를 작성하라
    Signed-off-by: DevRunner21 <devrunner21@gmail.com>

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,9 +1,11 @@
 package study;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
 
@@ -27,6 +29,34 @@ public class StringTest {
 
         // then
         assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("charAt()는 지정한 index에 해당하는 문자를 char형으로 반환한다.")
+    void test_charAt_success() {
+        // given
+        String data = "abc";
+        int index = 1;
+
+        // when
+        char actual = data.charAt(index);
+
+        // then
+        assertThat(actual).isEqualTo('b');
+    }
+
+    @Test
+    @DisplayName("charAt()는 지정한 index가 문자열의 길이를 벗어나면 IndexOutOfBoundsException를 던집니다.")
+    void test_charAt_IndexOutOfBoundsException() {
+        // given
+        String data = "abc";
+        int outOfIndex = data.length() + 2;
+
+        // when
+        // then
+        assertThatThrownBy(() -> data.charAt(outOfIndex))
+            .isInstanceOf(IndexOutOfBoundsException.class)
+            .hasMessageContaining("String index out of range: " + outOfIndex);
     }
 
 }
