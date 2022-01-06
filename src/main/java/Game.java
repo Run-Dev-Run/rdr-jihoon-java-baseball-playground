2a16a857e0382b396ac4a626a8a0d902a0b6f449
devrunner21 <devrunner21@gmail.com>
2022-01-06 11:36:59 +0900

    feat: 사용자의 3자리 난수 입력 기능을 구현하라
    - 입력값 validation check 메서드 구현

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,3 +1,4 @@
+
 public class Game {
 
     private Input input;
@@ -15,24 +16,43 @@ public class Game {
         output.showGameStartMessage();
 
 //        while (true) {
-            score = new Score();
+        score = new Score();
 
-            // 1. 플레이어는 숫자를 입력한다.
-            int inputNumber = input.inputNumber();
+        // 1. 플레이어는 숫자를 입력한다.
+        int inputNumber = inputPlayerNumber();
 
-            // 2. 컴퓨터는 난수를 생성한다.
+        // 2. 컴퓨터는 난수를 생성한다.
 
-            // 3. 각 입력 값을 비교하고 점수를 계산한다.
+        // 3. 각 입력 값을 비교하고 점수를 계산한다.
 
-            // 4. 결과를 출력한다.
+        // 4. 결과를 출력한다.
 
-            // 5. 결과에 따라 알맞는 UI로 이동한다.
-            // 5-1. 3 스트라이크라면 게임 Continue 메시지를 출력한다.
-            // 5-2. 아니면 게임 다시시작
+        // 5. 결과에 따라 알맞는 UI로 이동한다.
+        // 5-1. 3 스트라이크라면 게임 Continue 메시지를 출력한다.
+        // 5-2. 아니면 게임 다시시작
 
 //        }
 
         output.showGameEndMessage();
+    }
+
+    private int inputPlayerNumber() {
+        int inputNumber = 0;
+        while (!validateInputNumber(inputNumber)) {
+            inputNumber = input.inputNumber();
+        }
+        return inputNumber;
+    }
+
+    public boolean validateInputNumber(int inputNumber) {
+        String inputNumberStr = String.valueOf(inputNumber);
+
+        // 3자리가 아니거나 0이 포함되어 있다면 false
+        if (inputNumberStr.length() != 3 || inputNumberStr.contains("0")) {
+            return false;
+        }
+
+        return false;
     }
 
 }
--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -1,6 +1,6 @@
 public interface Input {
 
-    String REQUEST_INPUT_NUMBER = "숫자를 입력해 주세요 : ";
+    String REQUEST_INPUT_NUMBER = "숫자를 입력해 주세요(3자리) : ";
 
     int inputNumber();
 
