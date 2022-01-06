95372982453728b01f452611011b04679891f721
devrunner21 <devrunner21@gmail.com>
2022-01-06 13:42:38 +0900

    feat: 두 입력을 비교해 ?볼 ?스트라이크 여부 판정 로직을 구현하라

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -16,26 +16,26 @@ public class Game {
     public void start() {
         output.showGameStartMessage();
 
-//        while (true) {
-        score = new Score();
-
-        // 1. 플레이어는 숫자를 입력한다.
-        int inputNumber = inputPlayerNumber();
-
         // 2. 컴퓨터는 난수를 생성한다.
         int randomNumber = getRandomNumber();
+        System.out.println(randomNumber);
 
-        // 3. 각 입력 값을 비교하고 점수를 계산한다.
+        while (true) {
+            score = new Score();
 
-        // 4. 결과를 출력한다.
+            // 1. 플레이어는 숫자를 입력한다.
+            int inputNumber = inputPlayerNumber();
 
-        // 5. 결과에 따라 알맞는 UI로 이동한다.
-        // 5-1. 3 스트라이크라면 게임 Continue 메시지를 출력한다.
-        // 5-2. 아니면 게임 다시시작
+            // 3. 각 입력 값을 비교하고 점수를 계산한다.
+            score.calculateScore(inputNumber, randomNumber);
 
-//        }
+            // 4. 결과를 출력한다.
+            output.showMessage(score.getResultString());
 
-        output.showGameEndMessage();
+            // 5. 결과에 따라 알맞는 UI로 이동한다.
+
+        }
+
     }
 
     private int inputPlayerNumber() {
--- src/main/java/Score.java
+++ src/main/java/Score.java
@@ -17,4 +17,49 @@ public class Score {
         return ballCount;
     }
 
+    public void calculateScore(int inputNumber, int randomNumber) {
+
+        if (inputNumber == randomNumber) {
+            strikeCount = 3;
+            return;
+        }
+
+        String inputNumberStr = String.valueOf(inputNumber);
+        String randomNumberStr = String.valueOf(randomNumber);
+
+        for (int i = 0; i < inputNumberStr.length(); i++) {
+            String keyword = String.valueOf(inputNumberStr.charAt(i));
+            checkHasBallOrStrike(randomNumberStr, i, keyword);
+        }
+
+    }
+
+    public String getResultString() {
+        StringBuilder resultStr = new StringBuilder("");
+
+        if (ballCount > 0) {
+            resultStr.append(ballCount + "볼 ");
+        }
+
+        if (strikeCount > 0) {
+            resultStr.append(strikeCount + "스트라이크");
+        }
+
+        return resultStr.toString();
+    }
+    private void checkHasBallOrStrike(String randomNumberStr, int index, String keyword) {
+        int foundIndex = randomNumberStr.indexOf(keyword);
+
+        if (foundIndex < 0) {
+            return;
+        }
+
+        if (foundIndex != index) {
+            ballCount++;
+            return;
+        }
+
+        strikeCount++;
+    }
+
 }
