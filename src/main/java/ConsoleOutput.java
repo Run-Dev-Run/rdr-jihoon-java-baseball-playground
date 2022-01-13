ca8f51b14f345e8294af04d417b9482b6399a529
devrunner21 <devrunner21@gmail.com>
2022-01-13 16:42:32 +0900

    [refactor] 출력 메시지를 상수로 전환하라

--- src/main/java/ConsoleOutput.java
+++ src/main/java/ConsoleOutput.java
@@ -1,8 +1,10 @@
 public class ConsoleOutput implements Output{
 
+    public static final String RESULT_MESSAGE = "결과 = ";
+
     @Override
     public void showResult(int result) {
-        System.out.println("결과 = " + result);
+        System.out.println(RESULT_MESSAGE + result);
     }
 
 }
